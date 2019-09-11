689e15b3828532b9a30b2412cbb62136c7a9ffe5
Luca <luc4.ruggieri@gmail.com>
2019-09-11 20:33:32 +0200

    install.sh updated with gcc and g++ requirements

--- install.sh
+++ install.sh
@@ -32,7 +32,6 @@ isDistro () {
     for dist in "${distros[@]}";
     do
         if [[ "${os}" == *"${dist}"* ]]; then
-            echo "OK $os $dist"
             return 0
         fi
     done
@@ -45,31 +44,54 @@ OS="$(/bin/bash getOS.sh)"
 
 if isDistro "$OS" "${DISTRO_REDHAT[@]}"; then
     shw_info "Red Hat distribution '$OS' detected"
-    
 
-    shw_norm "Installing epel-release..."
-    if yum installed "epel-release" >/dev/null 2>&1; then
-        shw_info "epel-release" already installed
+    #gcc and g++ are necessary for gosseract and underling libraries
+    if command -v gcc >/dev/null 2>&1  ; then
+     shw_norm "gcc already installed"
     else
+        sudo yum install -y gcc
+    fi
+    if command -v g++ >/dev/null 2>&1  ; then
+     shw_norm "g++ already installed"
+    else
+        sudo yum install -y gcc-c++
+    fi
+
+
+    if yum list installed "epel-release" >/dev/null 2>&1; then
+        shw_norm "epel-release already installed"
+    else
+        shw_norm "Installing epel-release..."
         sudo yum install -y epel-release
         shw_info "epel-release installed"
     fi
 
 
-    shw_norm "Installing tesseract (with leptonica)..."
     if command -v tesseract >/dev/null 2>&1  ; then
         shw_norm "tesseract already installed"
     else
+        shw_norm "Installing tesseract (with leptonica)..."
         sudo yum install -y tesseract-devel leptonica-devel
         shw_info "tesseract with leptonica installed"
     fi
-elif isDistro "$OS" "${$DISTRO_UBUNTU[@]}"; then
+elif isDistro "$OS" "${DISTRO_UBUNTU[@]}"; then
     shw_info "Ubuntu distribution '$OS' detected"
 
-    shw_norm "Installing tesseract with leptonica..."
+    #gcc and g++ are necessary for gosseract and underling libraries
+    #on Ubuntu, installing build-essential suffice
+    if [ $(dpkg-query -W -f='${Status}' build-essential 2>/dev/null | grep -c "ok installed") -eq 0 ];
+    then
+        shw_info "Installing build-essential"
+        sudo apt install build-essential -y
+    else
+        shw_norm "build-essential already installed"
+    fi
+
+
     if command -v tesseract >/dev/null 2>&1  ; then
         shw_norm "tesseract already installed"
     else
+        shw_norm "Installing tesseract with leptonica..."
         sudo apt-get install -y libtesseract-dev libleptonica-dev tesseract-ocr-eng
         shw_info "tesseract with leptonica installed"
     fi
@@ -90,9 +112,9 @@ fi
 
 #Now, proceed with Go dependencies installation; this step uses dep (https://github.com/golang/dep)
 if command -v $GOPATH/bin/dep >/dev/null 2>&1 ; then
-    shw_info "Dep already installed"
+    shw_norm "Dep already installed"
 else
-    shw_warn "Dep not installed! Installing..."
+    shw_info "Dep not installed! Installing..."
 
     #dep requires folder $GOPATH/bin to exist
     if [[ ! -d "$GOPATH/dep" ]]; then
