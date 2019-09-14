e990b34ee27fcb8fbbe48daad4aba865ab1b7699
Luca Ruggieri <luc4.ruggieri@gmail.com>
2019-09-14 11:54:17 +0000

    crawler ready. Captcha page resolution ready. Cookies ready to not trigger captcha page. At the moment it only prints basic info. Still to be completed

---
+++ cmd/visual_resource_server/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+	"gitlab.com/ruggieri/gonema/pkg/utils"
+	"gitlab.com/ruggieri/gonema/pkg/visual_resource"
+	"log"
+	"net/http"
+)
+
+func main(){
+	utils.DebugActive = true
+	utils.Logger.Level = logrus.DebugLevel
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/",emptyRequest)
+	mux.HandleFunc("/resourceInfo",resourceInfo)
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
+
+func emptyRequest(w http.ResponseWriter, r *http.Request){
+	http.NotFound(w,r)
+}
+
+func resourceInfo(w http.ResponseWriter, r *http.Request){
+	requestParameters:= r.URL.Query()
+	if imdbID:= requestParameters.Get("imdbID") ; len(imdbID) > 0{
+		resource,err := visual_resource.GetResource("",imdbID)
+		if err != nil{
+			dealWithInternalError(w,err)
+		}else{
+			respond(w,http.StatusOK,[]byte(resource.String()))
+		}
+	}else if resourceTitle := requestParameters.Get("resourceTitle") ; len(resourceTitle) > 0{
+		respond(w,http.StatusOK,[]byte("Sorry, resourceTitle not handled yet"))
+	}else{
+		respond(w,http.StatusBadRequest,[]byte("Please, specify 'imdbID' or 'resourceTitle'"))
+	}
+}
+
+func dealWithInternalError(w http.ResponseWriter, iErr error){
+	utils.Logger.Error(iErr)
+	http.Error(w, "" +
+		"We are very sorry, but something on our side has broken. " +
+		"This issue has been reported and will be dealt as soon as possible by our engineering team.", http.StatusInternalServerError)
+}
+func respond(w http.ResponseWriter, iStatusCode int, iMessage []byte){
+	w.WriteHeader(iStatusCode)
+	_, err := w.Write(iMessage)
+	if err != nil {
+		dealWithInternalError(w,err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,14 @@
+module gitlab.com/ruggieri/gonema
+
+go 1.12
+
+require (
+	github.com/chromedp/cdproto v0.0.0-20190908230719-576f726fb49f
+	github.com/chromedp/chromedp v0.4.0
+	github.com/davecgh/go-spew v1.1.1
+	github.com/nmmh/magneturi v0.0.0-20180607142838-464c677b3fb5
+	github.com/otiai10/gosseract v2.2.1+incompatible
+	github.com/otiai10/mint v1.3.0 // indirect
+	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/net v0.0.0-20190912160710-24e19bdeb0f2 // indirect
+)
